b83a8d74c25952f8bb786efe7189c463476b6370
Robinzon <robinzon.sokhashvili@gmail.com>
2018-08-24 19:09:05 +0400

    contacts

--- php_files/contact_form.php
+++ php_files/contact_form.php
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 
     if (isset($_POST['submit'])){
@@ -25,6 +26,33 @@
 
 
 
+=======
+<?php
+
+    if (isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $tel = $_POST['telephone'];
+        $email = $_POST['email'];
+        $org = $_POST['organizationn_name'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $mail_to = "rsokhashvili@gtgroupe.ge";
+        $headers = "ვისგან: " . $email . ".\n"
+                 . "სახელი : " . $name . "\n" 
+                 . ". ორგანიზაციის სახელი : " . $org . 
+                 ".\n\n";
+        $txt = "შეტყობინება : " .  $message;
+
+
+        mail($mail_to, $subject, $txt, $headers);
+        header("Location : index.php?mailsend");
+
+    }
+
+
+
+>>>>>>> 1d0683f38d8e62d17de5a0a4c59101daef2a3798
 =======
 <?php
 
